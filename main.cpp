3fa269c2ecefaeb8df2a1c56c621cf5e52d43b7b
Ilya1810 <ilayl70245@gmail.com>
2024-12-07 22:08:12 +0500

    Добавлено чтение данных из файла

--- main.cpp
+++ main.cpp
@@ -9,23 +9,35 @@ using namespace std;
 //Установить 1-байтовое выравнивание
 #pragma pack(push, 1)
 
-// Объявление структуры
+// Объявление структуры для общей части заголовка
 struct rar_header
 {
     uint16_t header_crc;
     uint8_t header_type;
     uint16_t header_flag;
     uint16_t header_size;
-   // char padding[8]; // Пропуск 8 байт (просто чтобы показать)
 };
 
-//Восстановить выравнивание
+// Восстановить выравнивание
 #pragma pack(pop)
 
 int main()
 {
-
     setlocale (LC_ALL, "Russian");
+
+    ifstream rar_file("Example.rar", ios::binary);
+    if (rar_file.is_open())
+    {
+        // Определить размер файла
+        rar_file.seekg(0, ios::end);
+        int rar_size = rar_file.tellg();
+        rar_file.seekg(0, ios::beg);
+        cout << rar_size << endl;
+
+        // Считать данные из файла
+        vector<char> rar_data(rar_size, 0);
+        rar_file.read(rar_data.data(), rar_size);
+    }
 
     return 0;
 }
