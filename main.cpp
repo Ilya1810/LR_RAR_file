834bc5e81b62f6dc610ed7d98086aa7a0234898d
Ilya1810 <ilayl70245@gmail.com>
2024-12-07 21:40:35 +0500

    Добавлено объявление структуры, сортирующей заголовок фалйла RAR4

--- main.cpp
+++ main.cpp
@@ -6,8 +6,26 @@
 
 using namespace std;
 
+//Установить 1-байтовое выравнивание
+#pragma pack(push, 1)
+
+// Объявление структуры
+struct rar_header
+{
+    uint16_t header_crc;
+    uint8_t header_type;
+    uint16_t header_flag;
+    uint16_t header_size;
+   // char padding[8]; // Пропуск 8 байт (просто чтобы показать)
+};
+
+//Восстановить выравнивание
+#pragma pack(pop)
+
 int main()
 {
+
+    setlocale (LC_ALL, "Russian");
 
     return 0;
 }
